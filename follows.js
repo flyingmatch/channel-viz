70fac915ddd57df92cff55b0c4da8ae4978863a7
flyingmatch <flyingmatch@gmail.com>
2014-01-02 18:07:37 -0800

    Update follows.js

--- follows.js
+++ follows.js
@@ -148,7 +148,7 @@
 										element: document.querySelector('#graph-' + feedId + '-' + datastream.id),
 										width: 600,
 										height: 200,
-										renderer: 'line',
+										renderer: 'bar',
 										min: parseFloat(datastream.min_value) - .25*(parseFloat(datastream.max_value) - parseFloat(datastream.min_value)),
 										max: parseFloat(datastream.max_value) + .25*(parseFloat(datastream.max_value) - parseFloat(datastream.min_value)),
 										padding: {
@@ -157,7 +157,7 @@
 											bottom: 0.02,
 											left: 0.02
 										},
-										series: series
+										series: [series, series]
 									});
 
 									graph.render();
